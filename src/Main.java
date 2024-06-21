156f7839889660f12bc72211ac9c8dcb3b6fedd9
Antoha-Sychev <antoha.sychev@yandex.ru>
2024-06-21 19:34:14 +0500

    Startind HM 2

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+public class Main
+{ public static void main(String[] args)
+        {
+        var Dog = 8.0;
+        var Cat = 3.6;
+        var Paper = 763789;
+        System.out.println("Задача 1");
+        System.out.println("Dog" + Dog);
+        System.out.println("Cat" + Cat);
+        System.out.println("Paper" + Paper);
+
+        System.out.println("Задача 2");
+        System.out.println("Dog"+(Dog + 4));
+        System.out.println("Cat"+(Cat + 4));
+        System.out.println("Paper"+(Paper + 4));
+
+        System.out.println("Задача 3");
+        System.out.println("Dog"+((Dog + 4)-3.5));
+        System.out.println("Cat"+((Cat + 4)-1.6));
+        System.out.println("Paper"+((Paper + 4)-7639));
+
+
+        System.out.println("Задача 4");
+        var friend = 19;
+        System.out.println("friend"+friend);
+        System.out.println("friend"+(friend + 2));
+        System.out.println("friend"+((friend + 2)/7));
+
+        System.out.println("Задача 5");
+        var frog = 3.5;
+        System.out.println("frog"+frog);
+        System.out.println("frog"+(frog*10));
+        System.out.println("frog"+((frog*10)/3.5));
+        System.out.println("frog"+(((frog*10)/3.5)+4));
+        }
+}
