b3adab640c864bd9d871fead0c50c0eab83ee867
Admin <antoha.sychev@ynadex.ru>
2024-07-09 21:56:36 +0500

    Homework 3 task 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет Мир!");
-        System.out.println("Hellow hellow World!");
+        System.out.println("Задание 1");
+        System.out.println(" ");
+        byte b = 34;
+        short c = -13475;
+        int i = 5245872;
+        long l = 987896549;
+        float f = 3.14159265f;
+        double d = 56.1365433573;
+        System.out.println("b = "+b);
+        System.out.println("c = "+c);
+        System.out.println("i = "+i);
+        System.out.println("l = "+l);
+        System.out.println("f = "+f);
+        System.out.println("d = "+d);
+        System.out.println(" ");
+        System.out.println("Задание 2");
+        System.out.println(" ");
+
+
+
     }
 }
