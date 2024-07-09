803e22aefbb7234e0e96ba6901402b3e33f49cd0
Admin <antoha.sychev@ynadex.ru>
2024-07-09 23:01:08 +0500

    Homework 3 task 1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задание 1");
+        System.out.println("Задание 1:");
         System.out.println(" ");
         byte b = 34;
         short c = -13475;
-        int i = 5245872;
-        long l = 987896549;
+        int i = -2084457672;
+        long l = 346779547468468468L;
         float f = 3.14159265f;
         double d = 56.1365433573;
         System.out.println("b = "+b);
@@ -15,10 +15,33 @@ public class Main {
         System.out.println("f = "+f);
         System.out.println("d = "+d);
         System.out.println(" ");
-        System.out.println("Задание 2");
+        System.out.println("Задание 2:");
         System.out.println(" ");
-
-
-
+        double Double = 27.12;
+        long Long = 987678965549L;
+        float Float = 2.786f;
+        short oneShort = 569;
+        short twoShort = -159;
+        short threeShort = 27897;
+        byte Byte = 67;
+        System.out.println("double = "+Double);
+        System.out.println("Long = "+Long);
+        System.out.println("Float = "+Float);
+        System.out.println("OneShort = "+oneShort);
+        System.out.println("TwoShort = "+twoShort);
+        System.out.println("ThreeShort = "+threeShort);
+        System.out.println("Byte = "+Byte);
+        System.out.println(" ");
+        System.out.println("Задание 3:");
+        System.out.println(" ");
+        byte OneByte = 23;
+        byte TwoByte = 27;
+        byte ThreeByte = 30;
+        short allPaper = 480;
+        System.out.println("480/(23+27+30) = 6");
+        System.out.println("На каждого ученика рассчитано "+(allPaper/(OneByte+TwoByte+ThreeByte))+" листов бумаги. ");
+        System.out.println(" ");
+        System.out.println("Задание 4:");
+        System.out.println(" ");
     }
 }
