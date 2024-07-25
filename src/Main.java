51d2b9778620b963110643abfa36c6467205126c
Admin <antoha.sychev@ynadex.ru>
2024-07-25 09:59:10 +0500

    HW 5

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,40 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Main {
     public static void main(String[] args) {
-      char clientOS = '1';
-      switch (clientOS) {
-          case '1':
-              System.out.println("Установите версию приложения для Android по ссылке");
-              break;
-          case '0':
-              System.out.println("Установите версию приложения для iOS по ссылке");
-              break;
+        // Заданеи 1
+        {
+            char clientOS = '1';
+            switch (clientOS) {
+                case '1':
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                    break;
+                case '0':
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                    break;
+            }
 
-      }
+        }
+        // Задание 2
+
+        {
+            char clientOS = '1';
+            int age = 2018;
+            switch (clientOS) {
+                case '1':
+                    if (age > 2015) {
+                        System.out.println("Установите полную версию для Android по ссылке");
+                    }else {
+                        System.out.println("Установите облегченную версию для Android по ссылке");
+                    }
+                    break;
+                case '0':
+                     if (age > 2015){
+                         System.out.println("Установите полную версию приложения для iOS по ссылке");
+                     }else {
+                         System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                     }
+                    break;
+            }
+        }
     }
 }
