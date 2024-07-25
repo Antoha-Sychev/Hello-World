8a9328ec16fb2f82b70cdd93761749f62dc2d11c
Admin <antoha.sychev@ynadex.ru>
2024-07-25 09:20:15 +0500

    HW 5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет Мир!");
+       char color = 'l';
+       switch (color) {
+           case 'r':
+               System.out.println("Красный! Ехать нельзя.");
+               break;
+           case 'y':
+               System.out.println("Желтый. Внимание.");
+               break;
+           case 'g':
+               System.out.println("Зеленый! Проезд разрешен.");
+               break;
+           case 'l':
+               System.out.println("Поворот налево.");
+               break;
+           default:
+               System.out.println("Светофор сломался.");
+       }
     }
 }
