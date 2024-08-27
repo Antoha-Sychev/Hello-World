a9aa090ada3b3c214d4dbf202e6a890f61419c13
Ma4eTe <antoha.sychev@yandex.ru>
2024-08-27 21:58:54 +0500

    HW-8

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,37 @@
 public class Main {
     public static void main(String[] args) {
 
+        // Task 1
 
+        int [] weight = new int [12];
+        weight[0] = 1;
+        weight[1] = 2;
+        weight[2] = 3;
+
+        double [] fractions = {1.57, 7.654, 9.986};
+
+        long [] weightLong = new long [12];
+        weightLong[0] = 1894982579852L;
+        weightLong[1] = 12856926894222L;
+        weightLong[2] = 1294982579852L;
+        System.out.println(" ");
+
+        // Task 2
+
+        for (int i = 0; i < 12; i++) {
+            System.out.print(weight[i]+", ");
+        }
+        System.out.println(" ");
+        System.out.println(" ");
+
+        for (int i = 0; i < 3; i++){
+            System.out.print(fractions[i] + ", ");
+        }
+        System.out.println(" ");
+        System.out.println(" ");
+
+        for (int i = 0; i < 12; i++) {
+            System.out.print(weightLong[i]+", ");
+        }
     }
 }
