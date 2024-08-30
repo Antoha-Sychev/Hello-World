2561be93ffadd17874d7b80f538576aa352935ce
Admin <antoha.sychev@ynadex.ru>
2024-08-30 19:45:10 +0500

    HW-11

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
+        // Task 1
+
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+
+        System.out.println(" ");
+
+        // Task 2
+
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета - " + fullName.toUpperCase());
+
+        System.out.println(" ");
+
+        // Task 3
+
+        String fullName2 = fullName.replace("ё", "е");
+        System.out.println("Данные Ф.И.О. сотрудника - " + fullName2);
     }
 }
