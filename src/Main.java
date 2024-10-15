04bc34e24482b75bf8208ec1d36647f947fa0b71
Ma4eTe <antoha.sychev@yandex.ru>
2024-10-15 19:01:11 +0500

    HW-12

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-    }
+    public static void main(String[] args) {}
 
+    public static void isYearLeap(int year){
+        if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+            System.out.println(year + "год високосный");
+        }else{
+            System.out.println(year + "год не является високосным");
+        }
+        }
 }
