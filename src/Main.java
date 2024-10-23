ac9fa1afc750152e7758c028429c735161b8340a
Ma4eTe <antoha.sychev@yandex.ru>
2024-10-23 19:27:00 +0500

    HW-12

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        isYearLeap(2024);
         isYearLeap(2025);
+
+        checkApplicationVersion(1, 2019);
     }
+    // Task 1
 
     public static void isYearLeap(int year){
         if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
@@ -11,4 +15,24 @@ public class Main {
             System.out.println(year + " год не високосный");
         }
         }
+
+    // Task 2
+
+    public static void checkApplicationVersion (int os, int clientDeviceYear){
+        int currentYear = LocalDate.now().getYear();
+
+        if (os == 0) {
+            if (clientDeviceYear <= currentYear){
+                System.out.println("Установите облегченную версию приложения для IOS по ссылке");
+            }else{
+                System.out.println("Установите обычную версию приложения для IOS по ссылке");
+            }
+        }else if (os == 1){
+            if (clientDeviceYear <= currentYear){
+                System.out.println("Установите облеченную версию приложения для Android по ссылке");
+            }else{
+                System.out.println("Установите обычную версию приложения для Android по ссылке");
+            }
+        }
+    }
 }
